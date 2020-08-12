e5e3d959760d05f6a1eae2e1e28ec85c338bb254
Tchane821 <googldu59@gmail.com>
2020-08-12 16:41:55 +0200

    first contente

--- sitePcMontage/public_html/js/main.js
+++ sitePcMontage/public_html/js/main.js
@@ -1,1 +1,7 @@
 //init js
+const pres =  document.getElementsByClassName("pres");
+const conf =  document.getElementsByClassName("conf");
+const pc =  document.getElementsByClassName("pc");
+const tarif =  document.getElementsByClassName("tarif");
+
+pres.style.display = false;
